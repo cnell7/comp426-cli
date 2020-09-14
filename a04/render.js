f8e5987a20219db0500688becb6989e4f4efe043
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-09-13 22:15:01 -0400

    Finished

--- a04/render.js
+++ a04/render.js
@@ -28,12 +28,20 @@ export const renderHeroCard = function (hero) {
   let heroName = $("<span style='color:grey'></span>").append(
     "Hero = " + hero.name
   );
+  let firstSeen = $("<h2></h2>").append(
+    hero.firstSeen.getMonth() +
+      "/" +
+      hero.firstSeen.getDate() +
+      "/" +
+      hero.firstSeen.getFullYear()
+  );
   let heroDesc = $("<p style='color: white'></p>").append(hero.description);
   let button = $("<button class = 'button is-dark'>Edit</button>");
   returnHTML
     .append(img)
     .append(firstName)
     .append(heroName)
+    .append(firstSeen)
     .append(heroDesc)
     .append(button);
   return returnHTML;
@@ -49,6 +57,63 @@ export const renderHeroEditForm = function (hero) {
   // TODO: Generate HTML elements to represent the hero edit form
   // TODO: Return these elements as a string, HTMLElement, or jQuery object
   // Example: return `<form>${hero.name}</form>`;
+  //hero.first
+  //hero.last
+  // hero.name
+  //hero.description
+  //hero.firstSeen - The month and year of the first comic book issue in which the hero appears, stored as a JavaScript Date object
+  let $form = $("<form class = 'form'></form>");
+  let $first = $("<div class = 'field'></div>")
+    .append($("<label class = 'label'>First Name</label>"))
+    .append($('<div class = "control"></div>'))
+    .append(
+      $('<input class = "input" type="text" value=' + hero.first + "></input>")
+    );
+  let $last = $("<div class = 'field'></div>")
+    .append($("<label class = 'label'>Last Name</label>"))
+    .append($('<div class = "control"></div>'))
+    .append(
+      $('<input class = "input" type="text" value=' + hero.last + "></input>")
+    );
+  let $name = $("<div class = 'field'></div>")
+    .append($("<label class = 'label'>Hero Name</label>"))
+    .append($('<div class = "control"></div>'))
+    .append(
+      $('<input class = "input" type="text" value="' + hero.name + '"></input>')
+    );
+  let $desc = $("<div class = 'field'></div>")
+    .append($("<label class = 'label'>Description</label>"))
+    .append($('<div class = "control"></div>'))
+    .append(
+      $("<textarea class = 'textarea'>" + hero.description + "</textarea>")
+    );
+  let $firstSeen = $("<div class = 'field'></div>")
+    .append($("<label class = 'label'>First Seen</label>"))
+    .append($('<div class = "control"></div>'))
+    .append(
+      $(
+        "<input class = input value = " +
+          hero.firstSeen.getMonth() +
+          "/" +
+          hero.firstSeen.getFullYear() +
+          "></input>"
+      )
+    );
+  let $save = $("<div class = 'field'></div>")
+    .append($('<div class = "control"></div>'))
+    .append($("<button class='button' type = 'submit'>Save</button>"));
+  let $cancel = $("<div class = 'field'></div>")
+    .append($('<div class = "control"></div>'))
+    .append($("<button class='button' type = 'submit'>Cancel</button>"));
+  $form
+    .append($first)
+    .append($last)
+    .append($name)
+    .append($desc)
+    .append($firstSeen)
+    .append($save)
+    .append($cancel);
+  return $form;
 };
 
 /**
@@ -59,7 +124,8 @@ export const renderHeroEditForm = function (hero) {
 export const loadHeroesIntoDOM = function (heroes) {
   // Grab a jQuery reference to the root HTML element
   const $root = $("#root");
-  let $col = $("<div class='columns is-multiline'></div>").appendTo($root);
+  let $div1 = $("<div class = 'section'></div>").appendTo($root);
+  let $col = $("<div class='columns is-multiline'></div>").appendTo($div1);
   // TODO: Generate the heroes using renderHeroCard()
   // TODO: Append the hero cards to the $root element
   for (let i = 0; i < heroes.length; i++) {
@@ -69,7 +135,8 @@ export const loadHeroesIntoDOM = function (heroes) {
   const randomHero = heroes[Math.floor(Math.random() * heroes.length)];
 
   // TODO: Generate the hero edit form using renderHeroEditForm()
-
+  let $div2 = $("<div class = 'section'></div>").appendTo($root);
+  $div2.append(renderHeroEditForm(randomHero));
   // TODO: Append the hero edit form to the $root element
 };
 
