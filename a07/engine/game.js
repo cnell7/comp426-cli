8e38d8048025efb5b29742d4101469064d22c70b
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-19 16:36:15 -0400

    N/A

--- a07/engine/game.js
+++ a07/engine/game.js
@@ -1,3 +1,30 @@
-/*
-Add your code for Game here
- */
+export class Game {
+
+    constructor(l){
+        this.length = l
+    }
+    setupNewGame(){
+
+    }
+    loadGame(gameState){
+
+    }
+    move(direction){
+
+    }
+    toString(){
+
+    }
+    onMove(callback){
+
+    }
+    onWin(callback){
+
+    }
+    onLose(callback){
+
+    }
+    getGameState(){
+        
+    }
+}
