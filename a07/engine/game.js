e426ea42ac690e9865c828e655ea09e7148b3ecf
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-10-19 17:23:25 -0400

    Added more to the constructor and helper methods

--- a07/engine/game.js
+++ a07/engine/game.js
@@ -1,19 +1,43 @@
-export class Game {
+export default class Game {
 
     constructor(l){
-        this.length = l
+        this.length = l;
+        this.board = [];
+        this.score = 0;
+        this.won = false;
+        this.over = false;
+        this.setupNewGame()
     }
     setupNewGame(){
-
+        let count = 0;
+        let len = Math.pow(this.length, 2);
+        let firstOne = 0;
+        let firstTwo = 0;
+        while(firstOne == firstTwo){
+            firstOne = parseInt(Math.random() * len);
+            firstTwo = parseInt(Math.random() * len);
+        }
+        while(count < len){
+            if(count == firstOne || count == firstTwo){
+                this.board[count] = this.getRandom();
+            }
+            else{
+                this.board[count] = 0;
+            }
+            count++;
+        }
     }
     loadGame(gameState){
-
+        this.board = gameState.board;
+        this.score = gameState.score;
+        this.won = gameState.won;
+        this.over = gameState.over;
     }
     move(direction){
 
     }
     toString(){
-
+        return this.board;
     }
     onMove(callback){
 
@@ -26,5 +50,15 @@ export class Game {
     }
     getGameState(){
         
+    }
+
+    /*
+    HELPER METHODS
+    */
+    getRandom(){
+        if(Math.random() < .1){
+            return 4;
+        }
+        return 2;
     }
 }
