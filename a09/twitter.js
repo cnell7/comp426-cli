6c397a9c1cb9a2e82c70d4f1fed472d6f2088a7f
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-12 12:12:26 -0500

    Fixed map issue and add style

--- a09/twitter.js
+++ a09/twitter.js
@@ -1,51 +1,51 @@
-$( document ).ready(async function() {
+$( document ).ready(function() {
     let $root = $('#root');
     $root.append("<button id='createTweet' class='button'>Create</button>");
     $root.on("click", "#createTweet", function (e) {createHandler(e)});
     loadIndex();
     $root.on("click", ".like", function(e) {likeTweet(this.id)});;
     $root.on("click", ".retweet", function (e) {retweetTweet(this.id);});
-    $root.on("click", "#reply", function (e) {replyHandler(e);});
+    $root.on("click", ".reply", function (e) {replyHandler(this.id);});
     $root.on("click", ".editTweet", function (e) {editTweet(this.id)});
     return true;
 });
 
 async function loadIndex() {
     $('#tweets').remove();
-    let index = await axios({
-        method: 'get',
-        url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
-        withCredentials: true,
-    });;
-    let data = index.data
-    console.log(data);
+    let data = await getIndex();
+    data = data.data;
+    console.log(data)
     let $root = $('#root');
     let $indexAppend = $("<div id='tweets' class='container'>")
-    data.map(async function(element){
-        let type = element.type;
-        let tweet = await axios({
-            method: 'get',
-            url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets/'+element.id,
-            withCredentials: true,
-        });
+    let counter = 0;
+    while(counter < data.length){
+        let type = data[counter].type;
+        let tweet = await readTweet(data[counter].id);
         tweet = tweet.data;
         if(type == 'tweet'){
-            $indexAppend.append("<div id='"+tweet.id+"' class='message'><div class='message-header'><h1>"+tweet.author+"</h1></div><p class='tweetBody'>"+tweet.body+"</p><p id='likeCount'>"+tweet.likeCount +"  " + tweet.retweetCount+ "</p><button id='"+tweet.id+"'class='like button "+ checkIsMyLike(tweet) +"'>Like</button><button id='reply' class='button'>Reply</button><button id='"+element.id+"' class='retweet button'>Retweet</button>");
+            $indexAppend.append("<div id='"+tweet.id+"' class='message'><div class='message-header'><h1>"+tweet.author+"</h1></div><p class='tweetBody'>"+tweet.body+"</p><p id='likeCount'>"+tweet.likeCount +"  " + tweet.retweetCount+ "</p><button id='"+tweet.id+"'class='like button "+ checkIsMyLike(tweet) +"'>Like</button><button id='"+tweet.id+"' class='reply button'>Reply</button><button id='"+data[counter].id+"' class='retweet button'>Retweet</button>");
         }else if(type == 'retweet'){
-            let $retweetAppend = $("<h1 class='title is-5'>"+tweet.author+" Retweeted</h1>");
+            let $retweetAppend = $("<h1 class='retweetTitle title is-5'>"+tweet.author+" Retweeted</h1>");
             $retweetAppend.append("<h2>"+tweet.body+"</h2>");
             while(tweet.parent != null){
                 tweet = tweet.parent;
             }
-            $retweetAppend.append("<div class='container'><div id='"+tweet.id+"' class='message'><div class='message-header'><h1>"+tweet.author+"</h1></div><p>"+tweet.body+"</p><p id='likeCount'>"+tweet.likeCount +"  " + tweet.retweetCount+ "</p><button id='"+tweet.id+"'class='like button "+ checkIsMyLike(tweet) +"'>Like</button><button id='reply' class='button'>Reply</button><button id='"+element.id+"' class='retweet button'>Retweet</button>");
+            $retweetAppend.append("<div class='container'><div id='"+tweet.id+"' class='message'><div class='message-header'><h1>"+tweet.author+"</h1></div><p>"+tweet.body+"</p><p id='likeCount'>"+tweet.likeCount +"  " + tweet.retweetCount+ "</p><button id='"+tweet.id+"'class='like button "+ checkIsMyLike(tweet) +"'>Like</button><button id='"+tweet.id+"' class='reply button'>Reply</button><button id='"+data[counter].id+"' class='retweet button'>Retweet</button>");
             $indexAppend.append($retweetAppend);
         }else if(type == 'reply'){
-            
+            let $replyAppend = $("<h1 class='replyTitle title is-5'>"+tweet.author+" Replied</h1>");
+            $replyAppend.append("<h2>"+tweet.body+"</h2>");
+            while(tweet.parent != null){
+                tweet = tweet.parent;
+            }
+            $replyAppend.append("<div class='container'><div id='"+tweet.id+"' class='message'><div class='message-header'><h1>"+tweet.author+"</h1></div><p>"+tweet.body+"</p><p id='likeCount'>"+tweet.likeCount +"  " + tweet.retweetCount+ "</p><button id='"+tweet.id+"'class='like button "+ checkIsMyLike(tweet) +"'>Like</button><button id='"+tweet.id+"' class='reply button'>Reply</button><button id='"+data[counter].id+"' class='retweet button'>Retweet</button>");
+            $indexAppend.append($replyAppend);
         }
         if(tweet.isMine){
             $('#'+tweet.id).append("<button id='"+tweet.id+"' class='editTweet button'>Edit")
         }
-    });
+        counter++;
+    }
     $root.append($indexAppend);
     return true;    
 }
@@ -58,19 +58,18 @@ function refresh(){
     loadIndex();
     return true;
 }
-
-function getIndex() {
-    return new Promise((resolve, reject) =>{
-        try {
-            const result = axios({
-                method: 'get',
-                url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
-                withCredentials: true,
-            });
-            resolve(result);
-        } catch(error) {
-            reject(error);
-    }})
+async function loadIndex2(){
+    const data = await axios({
+        method: 'get',
+        url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
+        withCredentials: true,
+    });
+    const result = await data
+    return result
+}
+function getIndex(){
+    let data = loadIndex2();
+    return data
 }
 
 async function updateTweet(id, body){
@@ -107,28 +106,43 @@ async function createHandler(e){
     });
 }
 
-function readTweet(id){
-    return new Promise((resolve, reject) =>{
-        try{
-            const result = axios({
-                method: 'get',
-                url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets/'+id,
-                withCredentials: true,
-            });
-            resolve(result);
-        } catch(error){
-            reject(error);
-        }
+async function fetchTweet(id){
+    const result = await axios({
+        method: 'get',
+        url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets/'+id,
+        withCredentials: true,
     });
+    const ret = await result;
+    return ret
+}
+
+function readTweet(id){
+    let tweet = fetchTweet(id);
+    return tweet;
 }
 
 async function likeTweet(id){
+    let data = await readTweet(id)
+    if(data.data.isLiked){
+        return unLikeTweet(id);
+    }
     const result = await axios({
         method: 'put',
         url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets/'+id+'/like',
         withCredentials: true,
     });
     refresh();
+    return result;
+}
+
+async function unLikeTweet(id){
+    const result = await axios({
+        method: 'put',
+        url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets/'+id+'/unlike',
+        withCredentials: true,
+      });
+    refresh();
+    return result;
 }
 
 function retweetTweet(id){
@@ -163,6 +177,27 @@ async function editTweet(id){
         })
     }
 }
-function replyHandler(e){
-    console.log('reply');
+function replyHandler(id){
+    if($('#replyInput').length == 0){
+        $('#'+id).append('<form><input id="replyInput"></input><button id="submitReply">Submit</button></form>')
+        $('#root').on('click', "#submitReply", (e)=>{
+            e.preventDefault();
+            submitReply(id, $('#replyInput').val());
+        })
+    }
+}
+
+async function submitReply(id, body){
+    const result = await axios({
+        method: 'post',
+        url: 'https://comp426-1fa20.cs.unc.edu/a09/tweets',
+        withCredentials: true,
+        data: {
+          "type": "reply",
+          "parent": id,
+          "body": body
+        },
+      });
+      refresh();
+      return true;
 }
